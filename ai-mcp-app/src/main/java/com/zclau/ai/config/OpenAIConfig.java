67c7c435c978155d22078e41c6c0d16eaf4ed2bf
liuzicong <349856491@qq.com>
2025-08-08 17:59:51 +0800

    feat(dev-ops): project initialization

---
+++ ai-mcp-app/src/main/java/com/zclau/ai/config/OpenAIConfig.java
@@ -0,0 +1,42 @@
+package com.zclau.ai.config;
+
+import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class OpenAIConfig {
+    @Bean
+    public TokenTextSplitter tokenTextSplitter() {
+        return new TokenTextSplitter();
+    }
+
+
+    @Bean
+    public OpenAiApi openAiApi(@Value("${spring.ai.openai.base-url}") String baseUrl, @Value("${spring.ai.openai.api-key}") String apikey) {
+        return OpenAiApi.builder()
+                .baseUrl(baseUrl)
+                .apiKey(apikey)
+                .build();
+    }
+
+    @Bean("openAiSimpleVectorStore")
+    public SimpleVectorStore vectorStore(OpenAiApi openAiApi) {
+        OpenAiEmbeddingModel embeddingModel = new OpenAiEmbeddingModel(openAiApi);
+        return SimpleVectorStore.builder(embeddingModel).build();
+    }
+
+    @Bean("openAiPgVectorStore")
+    public PgVectorStore pgVectorStore(OpenAiApi openAiApi, JdbcTemplate jdbcTemplate) {
+        OpenAiEmbeddingModel embeddingModel = new OpenAiEmbeddingModel(openAiApi);
+        return PgVectorStore.builder(jdbcTemplate, embeddingModel)
+                .vectorTableName("vector_store_openai")
+                .build();
+    }
+}
